4ff358a52bbebc08ba723ea39f638af6bf8f7556
Jordan Duabe <me@jduabe.dev>
2024-11-22 23:28:11 +0800

    Add languageOptions to eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,18 @@
+import globals from "globals"
 import js from "@eslint/js"
 import eslintConfigPrettier from "eslint-config-prettier"
 
 export default [
   js.configs.recommended,
   eslintConfigPrettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
   {
     rules: {
       indent: ["error", 2],
